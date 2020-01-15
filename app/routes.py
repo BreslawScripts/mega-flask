947509b0637deafc104b255b1096f1305ae8510d
BreslawScripts <simon@breslawscripts.co.uk>
2020-01-15 21:21:38 +0000

    add template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello Simon!"
+    user = {'username': 'Simon'}
+    return render_template('index.html', title='Simon\'s homepage', user=user)
